91ad5a1125aeee6f4c005b5cbfdf49546f425db4
AnxQ <xiaoqi990112@hotmail.com>
2019-04-03 05:47:18 +0800

    ORM: Form submit

--- service/db.py
+++ service/db.py
@@ -127,9 +127,9 @@ class User(Base):
         pre.name = data['name']
         pre.school = data['school']
         pre.school_num = data['school_num']
-        pre.role = data['role']
+        pre.role = UserRole(data['role'])
         pre.tel = data['tel']
-        pre.gender = data['gender']
+        pre.gender = UserGender(data['gender'])
         pre.settings = data['settings']
         db_sess.commit()
 
--- service/handlers.py
+++ service/handlers.py
@@ -66,7 +66,7 @@ class UserHandler(BaseHandler):
         self.write(json.dumps(user.info_dict))
 
     @authenticated
-    def put(self):
+    def update(self):
         user: User = self.current_user
         try:
             update_data = json.loads(self.request.body)
